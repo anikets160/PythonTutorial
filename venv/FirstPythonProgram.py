a753efb54c619bc81ff75060180881faa6dfc750
Aniket S <anikets160@gmail.com>
2021-11-16 14:45:23 +0530

    Update FirstPythonProgram.py

--- venv/FirstPythonProgram.py
+++ venv/FirstPythonProgram.py
@@ -2,3 +2,21 @@ print("This is my first Python Program")
 
 print("This is my second programm")
 print("This is my third programm......")
+
+z,b,c = 1,2,"Aniket"
+
+i=10
+print(i)
+
+i="Aniket"
+print(i)
+
+i=70
+
+a=100.00
+print(a)
+
+print(i,"This is value of i")
+
+print(1,2,"abc",10.00)
+print(z,b,c)
